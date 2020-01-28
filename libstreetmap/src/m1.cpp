bc42ec3f23be6bc1ce985fcf17c186086f79282f
Justin Zhong <justinzhong7789@gmail.com>
2020-01-28 07:45:17 -0500

    fixed use of power in find_distance_between_two_points functino

--- libstreetmap/src/m1.cpp
+++ libstreetmap/src/m1.cpp
@@ -21,6 +21,8 @@
 #include "m1.h"
 #include "StreetsDatabaseAPI.h"
 #include <math.h>
+#include <vector>
+
 bool load_map(std::string /*map_path*/) {
     bool load_successful = false; //Indicates whether the map has loaded 
                                   //successfully
@@ -55,7 +57,7 @@ double find_distance_between_two_points(std::pair<LatLon, LatLon> points){
     double y2 = EARTH_RADIUS_METERS * points.second.lat() * DEGREE_TO_RADIAN;
     
     // c^2 = a^2 + b^2 -p
-    double result = sqrt((x2-x1)^2 + (y2-y1)^2);
+    double result = sqrt(pow((x2-x1),2) + pow((y2-y1),2));
     return result;
 }
 
@@ -65,65 +67,78 @@ double find_street_segment_length(int street_segment_id){
     InfoStreetSegment temp = getInfoStreetSegment(street_segment_id);
     // initialize total length to 0
     double length = 0;
-    // calculate distance from start to end
-    length = find_distance_between_two_points(getIntersectionPosition(temp.from), getIntersectionPosition(temp.to));
-    // THIS FUNCTION IS NOT COMPLETED, NEED TO ACCOUNT FOR CURVATURE
-    return length;
+    if(temp.curvePointCount==0){
+    
+        //put start and end LatLon into a pair
+        std::pair<LatLon, LatLon> start_n_end(getIntersectionPosition(temp.from), getIntersectionPosition(temp.to));
+        // calculate distance from start to end
+        length = find_distance_between_two_points(start_n_end);
+        // THIS FUNCTION IS NOT COMPLETED, NEED TO ACCOUNT FOR CURVATURE
+        return length;
+    }
+    else{
+        //add distances between end points and curve points
+        return length;
+    }
 }
 
 //Returns the travel time to drive a street segment in seconds 
 //(time = distance/speed_limit)
-//Justin
+//J
 double find_street_segment_travel_time(int street_segment_id){
     double travel_time;
-    travel_time=find_street_segment_length(street_segment_id)/getInfoStreetSegment(street_segment_id).speedLimit;
+    travel_time=find_street_segment_length(street_segment_id)/ (getInfoStreetSegment(street_segment_id).speedLimit);
     return travel_time;
 }
 
 //Returns the nearest intersection to the given position
 int find_closest_intersection(LatLon my_position){
     
+
 }
 
 //Returns the street segments for the given intersection 
 std::vector<int> find_street_segments_of_intersection(int intersection_id){
+   
     
+
 }
 
 //Returns the street names at the given intersection (includes duplicate street 
 //names in returned vector)
 std::vector<std::string> find_street_names_of_intersection(int intersection_id){
-    
+
 }
 
 //Returns true if you can get from intersection_ids.first to intersection_ids.second using a single 
 //street segment (hint: check for 1-way streets too)
 //corner case: an intersection is considered to be connected to itself
 bool are_directly_connected(std::pair<int, int> intersection_ids){
-    
+
 }
 
 //Returns all intersections reachable by traveling down one street segment 
 //from given intersection (hint: you can't travel the wrong way on a 1-way street)
 //the returned vector should NOT contain duplicate intersections
 std::vector<int> find_adjacent_intersections(int intersection_id){
+
     
 }
 
 //Returns all street segments for the given street
 std::vector<int> find_street_segments_of_street(int street_id){
-    
+
 }
 
 //Returns all intersections along the a given street
 std::vector<int> find_intersections_of_street(int street_id){
-    
+
 }
 
 //Return all intersection ids for two intersecting streets
 //This function will typically return one intersection id.
 std::vector<int> find_intersections_of_two_streets(std::pair<int, int> street_ids){
-    
+
 }
 
 //Returns all street ids corresponding to street names that start with the given prefix
@@ -134,19 +149,18 @@ std::vector<int> find_intersections_of_two_streets(std::pair<int, int> street_id
 //You can choose what to return if the street prefix passed in is an empty (length 0) 
 //string, but your program must not crash if street_prefix is a length 0 string.
 std::vector<int> find_street_ids_from_partial_street_name(std::string street_prefix){
-    
-}
 
+}
 //Returns the area of the given closed feature in square meters
 //Assume a non self-intersecting polygon (i.e. no holes)
 //Return 0 if this feature is not a closed polygon.
 double find_feature_area(int feature_id){
-    
-}
+
+}    
 
 //Returns the length of the OSMWay that has the given OSMID, in meters.
 //To implement this function you will have to  access the OSMDatabaseAPI.h 
 //functions.
 double find_way_length(OSMID way_id){
-    
+
 }
