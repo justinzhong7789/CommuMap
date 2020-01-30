b73d2a44c33bb65714d9fe0b7d4bac4750505432
Priscilla <priscilla.deng@mail.utoronto.ca>
2020-01-29 21:47:17 -0500

    Added function that loads streets connected to an intersection in Datastructures.cpp

--- libstreetmap/src/DataStructures.cpp
+++ libstreetmap/src/DataStructures.cpp
@@ -6,17 +6,35 @@
 
 #include "DataStructures.h"
 
-std::vector<LatLon> intersectionPoints;
-std::unordered_map<std::string, StreetIndex> StreetNames;
+std::vector<LatLon> intersectionTable;
+std::unordered_map<std::string, StreetIndex> StreetNamesTable;
+std::unordered_map<std::vector<int>, IntersectionIndex> Intersection_StreetTable;
 
+// allocates vector of intersection ids
 void makeIntersectionTable(){
-    for (IntersectionIndex id=0; id<getNumIntersections();id++){
-        intersectionPoints.push_back(getIntersectionPosition(id));
+    for (IntersectionIndex id=0; id<getNumIntersections(); id++){
+        intersectionTable.push_back(getIntersectionPosition(id));
+    }
+}
+// allocates map of all street names + street index
+void makeStreetNamesTable(){
+    for (StreetIndex id=0; id<getNumStreets();id++){
+        StreetNamesTable.insert({getStreetName(id), id});
+    }
+}
+// my implementation of find streets in intersection -p
+void makeIntersection_StreetTable(){
+    std::vector<int> streets_attached;
+    for (IntersectionIndex id=0; id<getNumIntersections(); id++){
+        for(int i=0; i < getIntersectionStreetSegmentCount(id); ++i){
+            // add each street connecting to the intersection into streets_attached vector
+            StreetSegmentIndex temp = getIntersectionStreetSegment(id, i);
+            streets_attached.push_back((getInfoStreetSegment(temp)).streetID);
+        }
+        // add vector and intersection id to the map
+        Intersection_StreetTable.insert({streets_attached, id});
+        // clear vector for next for loop
+        streets_attached.clear();
     }
 }
 
-void makeStreetNamesTable(){
-    for (int id=0; id<getNumStreets();id++){
-        StreetNames.insert({getStreetName(id), id});
-    }
-}
