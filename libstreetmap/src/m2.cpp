12f0e29f81e19bafb73ab3deb04afa31142bcaca
Priscilla <priscilla.deng@mail.utoronto.ca>
2020-02-19 01:32:49 -0500

    Drew the streets onto the map in black. Very preliminary, needs support for zoom.

--- libstreetmap/src/m2.cpp
+++ libstreetmap/src/m2.cpp
@@ -21,13 +21,13 @@
 
 #include <string>
 #include <vector>
-//mapBounds values
 double max_lat;
 double min_lat;
 double max_lon;
 double min_lon;
 
 void map_bounds();
+void draw_streets();
 void draw_main_canvas(ezgl::renderer *g);
 
 struct intersectionData {
@@ -35,7 +35,14 @@ struct intersectionData {
     std::string name;
 };
 
+struct streetSegmentsData {
+    // node takes the start, end, and curve points (if applicable) of the street segment
+    std::vector<LatLon> node;
+    std::string name;
+};
+
 std::vector <intersectionData> intersections;
+std::vector <streetSegmentsData> streetSegments;
 
 void draw_map(){
     ezgl::application::settings settings;
@@ -50,6 +57,7 @@ void draw_map(){
      intersections.resize(getNumIntersections());   
    
     map_bounds();
+    draw_streets();
     ezgl::rectangle initial_world({min_lon,min_lat}, {max_lon,max_lat});
 
     application.add_canvas("MainCanvas", draw_main_canvas, initial_world);
@@ -70,6 +78,16 @@ void draw_main_canvas(ezgl::renderer *g){
         
         g->fill_rectangle({x,y}, {x+width, y+height});
     }
+    for (size_t i=0; i<streetSegments.size(); i++){
+//    for (size_t i=0; i<1; i++){
+        for (size_t j=1; j<streetSegments[i].node.size(); j++){
+            std::pair <float, float> start = {streetSegments[i].node[j-1].lon(), streetSegments[i].node[j-1].lat()};
+            std::pair <float, float> end = {streetSegments[i].node[j].lon(), streetSegments[i].node[j].lat()};
+            g->set_color(ezgl::BLACK);
+            g->set_line_dash(ezgl::line_dash::none);
+            g->draw_line({start.first, start.second}, {end.first, end.second});
+        }
+    }
 }
 
 void map_bounds(){
@@ -89,4 +107,30 @@ void map_bounds(){
         min_lon = std::min(min_lon, intersections[id].position.lon());
     }
     
+}
+
+void draw_streets(){
+    streetSegments.resize(getNumStreetSegments());
+    for (StreetSegmentIndex id=0; id<streetSegments.size(); id++){
+        InfoStreetSegment info = getInfoStreetSegment(id);
+        streetSegments[id].node.resize(info.curvePointCount + 2);
+        // Find LatLon of beginning of intersection
+        streetSegments[id].node[0] = getIntersectionPosition(info.from);
+        // If no curve points, find LatLon of end of intersection
+        if (info.curvePointCount == 0){
+            streetSegments[id].node[1] = getIntersectionPosition(info.to);
+            // Connect the begin and end to form a street
+            
+        }
+        // Find all LatLons of each curve point
+        else {
+            for (int i=1; i<=info.curvePointCount+1; i++){
+                if (i>info.curvePointCount){
+                    streetSegments[id].node[i] = getIntersectionPosition(info.to);
+                } else {
+                streetSegments[id].node[i] = getStreetSegmentCurvePoint(i-1, id);
+                }
+            }
+        }
+    }
 }
