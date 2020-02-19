a06b37b7797215bdf5687ae212c27d3f3062f617
Marie Joy Cuevas <mariejoy.cuevas@mail.utoronto.ca>
2020-02-19 12:23:10 -0500

    Added handControl.cpp file (currently empty)

---
+++ libstreetmap/src/m2_helper/handControl.cpp
@@ -0,0 +1,13 @@
+#include "m1.h"
+#include "m2.h"
+#include "ezgl/application.hpp"
+#include "ezgl/graphics.hpp"
+#include "StreetsDatabaseAPI.h"
+#include "OSMDatabaseAPI.h"
+#include <string>
+#include <vector>
+
+
+
+
+/*========================Separation for MERGE error PREVENTRION ==============================*/
