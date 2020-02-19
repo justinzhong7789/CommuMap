3b110c6f5ca799f53a8355ee67b6c19bb42bc3bd
Priscilla <priscilla.deng@mail.utoronto.ca>
2020-02-18 20:08:00 -0500

    Added a fill rectangle function that SHOULD print a square in lower left of screen (see tutorial slides) but is not working

--- libstreetmap/src/m2.cpp
+++ libstreetmap/src/m2.cpp
@@ -8,16 +8,21 @@
 #include "m2.h"
 #include "ezgl/application.hpp"
 #include "ezgl/graphics.hpp"
+#include "StreetsDatabaseAPI.h"
+#include "OSMDatabaseAPI.h"
+
+#include <string>
+#include <vector>
 
 void draw_main_canvas(ezgl::renderer *g);
-/*
+
 struct intersectionData {
     LatLon position;
     std::string name;
-}
+};
 
-std::vector<intersectionData> intersections;
-*/
+std::vector <intersectionData> intersections;
+
 void draw_map(){
     ezgl::application::settings settings;
     
@@ -32,12 +37,25 @@ void draw_map(){
     application.add_canvas("MainCanvas", draw_main_canvas, initial_world);
     
     
-    //intersections.resize(getNumIntersections());
-    
-    
+    intersections.resize(getNumIntersections());
+    for (int id=0; id<getNumIntersections(); id++){
+        intersections[id].position = getIntersectionPosition(id);
+        intersections[id].name = getIntersectionName(id);
+    }
+
     application.run(nullptr, nullptr, nullptr, nullptr);
 }
 
 void draw_main_canvas(ezgl::renderer *g){
     g->draw_rectangle({0,0}, {1000,1000});
+    
+    for (size_t i=0; i>intersections.size(); i++){
+        float x = intersections[i].position.lon();
+        float y = intersections[i].position.lat();
+        
+        float width = 10;
+        float height = width;
+        
+        g->fill_rectangle({x,y}, {x+width, y+height});
+    }
 }
