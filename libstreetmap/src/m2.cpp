81dd38c9eb36fa80f4bc05f134f939bc7e79e1b0
Priscilla <priscilla.deng@mail.utoronto.ca>
2020-02-18 19:40:53 -0500

    Make preliminary window of map, only initialized blank canvas so far.

--- libstreetmap/src/m2.cpp
+++ libstreetmap/src/m2.cpp
@@ -4,8 +4,40 @@
  * and open the template in the editor.
  */
 
+#include "m1.h"
 #include "m2.h"
+#include "ezgl/application.hpp"
+#include "ezgl/graphics.hpp"
 
+void draw_main_canvas(ezgl::renderer *g);
+/*
+struct intersectionData {
+    LatLon position;
+    std::string name;
+}
+
+std::vector<intersectionData> intersections;
+*/
 void draw_map(){
+    ezgl::application::settings settings;
     
+    // Include headers
+    settings.main_ui_resource = "libstreetmap/resources/main.ui";
+    settings.window_identifier = "MainWindow";
+    settings.canvas_identifier = "MainCanvas";
+    
+    // Create EZGL application
+    ezgl::application application(settings);
+    ezgl::rectangle initial_world({0,0}, {1000,1000});
+    application.add_canvas("MainCanvas", draw_main_canvas, initial_world);
+    
+    
+    //intersections.resize(getNumIntersections());
+    
+    
+    application.run(nullptr, nullptr, nullptr, nullptr);
+}
+
+void draw_main_canvas(ezgl::renderer *g){
+    g->draw_rectangle({0,0}, {1000,1000});
 }
