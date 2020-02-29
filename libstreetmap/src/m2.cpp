497399f1275464a1f5e101f4e648711dcb12cfb8
Justin Zhong <justinzhong7789@gmail.com>
2020-02-28 20:59:26 -0500

    trivial change

--- libstreetmap/src/m2.cpp
+++ libstreetmap/src/m2.cpp
@@ -31,13 +31,14 @@
 #include <string>
 #include <set>
 #include <iostream>
-
+#include <camera.hpp>
 using namespace std;
 using namespace ezgl;
 
 void draw_main_canvas(ezgl::renderer *g);
-
-
+void initial_setup(ezgl::application *application, bool /*new_window*/);//add find button
+void find_button(GtkWidget */*widget*/, ezgl::application *application);
+void highlight_intersections(vector<int> intersection_ids, ezgl::renderer* g);
 //Determining the first time drawn
 int numTimesDrawn = 0;
 ezgl::color BACKGROUND(237,237,237);
@@ -67,7 +68,7 @@ void draw_map() {
     application.add_canvas("MainCanvas", draw_main_canvas, initial_world, BACKGROUND);
 
 
-    application.run(nullptr, act_on_mouse_click, nullptr, nullptr);
+    application.run(initial_setup, act_on_mouse_click, nullptr, nullptr);
 }
 
 void draw_main_canvas(ezgl::renderer *g) {
@@ -90,3 +91,63 @@ void draw_main_canvas(ezgl::renderer *g) {
 
 }
 
+void initial_setup(ezgl::application *application, bool /*new_window*/){
+    
+    application->create_button("find", 6, find_button);
+    
+}
+
+void find_button(GtkWidget */*widget*/, ezgl::application *application){
+    cout<< "find button is pressed."<< endl;
+    cout<< "Enter 2 street names below to find an intersection." << endl;
+    int street_id[2];
+    vector<int> intersection_ids;
+    
+    for(int i=0; i< 2;i++){ //enter 2 street names to find intersection
+        vector <int> street_ids;
+        string str;
+        getline(cin, str);
+        cout<< str<<endl;
+        street_ids = find_street_ids_from_partial_street_name(str);
+        for(int j=0; j< street_ids.size(); j++){
+            cout<<getStreetName(j)<<endl;
+        }
+        while(street_ids.size()!=1){
+            cout <<"The partial street name does not uniquely identify a street." <<endl
+                 <<"Please try again." << endl;
+            getline(cin, str);
+            street_ids = find_street_ids_from_partial_street_name(str);
+        }
+        street_id[i] = street_ids[0];
+    }
+    intersection_ids = find_intersections_of_two_streets(make_pair(street_id[0], street_id[1]));
+    highlight_intersections(intersection_ids, application->get_renderer());
+    
+    
+}
+                                   
+void highlight_intersections(vector<int> intersection_ids, ezgl::renderer *g){
+    rectangle recover_screen = g->get_visible_screen();
+    // this contains the screen when zoom_fit is clicked
+    g->set_color(YELLOW);
+    for(int i=0; i< intersection_ids.size(); i++){
+        LatLon intersection_position = getIntersectionPosition(intersection_ids[i]);
+        g->fill_arc({x_from_lon(intersection_position.lon()), y_from_lat(intersection_position.lat())}, 10, 0, 360);
+        cout<< "*******************Intersection "<<i+1 <<" ******************"<<endl
+            <<"Intersection id :           "<<endl<< intersection_ids[i]<< endl
+            <<"Intersection street names:  ";
+        vector<string> street_names =  find_street_names_of_intersection(intersection_ids[i]);
+        for(int j=0;j<street_names.size();j++){
+            cout<< street_names[j]<<endl;
+        }
+        cout<<"********************************************************"<< endl;
+    }
+    char YOrN;
+    cout<<"Done browing? y/n"<<endl;       
+    while(YOrN!='y'){
+        cin>> YOrN;
+    }
+    if(YOrN =='Y'){
+        g->set_visible_world(recover_screen);
+    }
+}
