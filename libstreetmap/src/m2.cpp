ea6451da84a71a5deb91b6794f9dfd258061562c
Justin Zhong <justinzhong7789@gmail.com>
2020-02-29 12:01:16 -0500

    made find button function take input from the search bar instead of cin

--- libstreetmap/src/m2.cpp
+++ libstreetmap/src/m2.cpp
@@ -46,6 +46,7 @@ void draw_main_canvas(ezgl::renderer *g);
 void initial_setup(ezgl::application *application, bool /*new_window*/);//add find button
 void find_button(GtkWidget */*widget*/, ezgl::application *application);
 void highlight_intersections(vector<int> intersection_ids, ezgl::renderer* g);
+string drawFindSearchBar(ezgl::renderer *g);
 //Determining the first time drawn
 int numTimesDrawn = 0;
 //ezgl::color BACKGROUND(237,237,237);
@@ -108,30 +109,20 @@ void initial_setup(ezgl::application *application, bool /*new_window*/){
 void find_button(GtkWidget */*widget*/, ezgl::application *application){
     cout<< "find button is pressed."<< endl;
     cout<< "Enter 2 street names below to find an intersection." << endl;
-    int street_id[2];
+    ezgl::renderer *g = application->get_renderer();
+    string two_streets[2];
+    int two_streets_id[2];
     vector<int> intersection_ids;
     
     for(int i=0; i< 2;i++){ //enter 2 street names to find intersection
-        vector <int> street_ids;
-        string str;
-        getline(cin, str);
-        cout<< str<<endl;
-        street_ids = find_street_ids_from_partial_street_name(str);
-        for(int j=0; j< street_ids.size(); j++){
-            cout<<getStreetName(j)<<endl;
-        }
-        while(street_ids.size()!=1){
-            cout <<"The partial street name does not uniquely identify a street." <<endl
-                 <<"Please try again." << endl;
-            getline(cin, str);
-            street_ids = find_street_ids_from_partial_street_name(str);
-        }
-        street_id[i] = street_ids[0];
+        two_streets[i] = drawFindSearchBar(g);
     }
-    intersection_ids = find_intersections_of_two_streets(make_pair(street_id[0], street_id[1]));
-    highlight_intersections(intersection_ids, application->get_renderer());
-    
-    
+    for(int i=0; i< getNumStreets();i++){
+        if(getStreetName(i)==two_streets[0]){two_streets_id[0]=1;}
+        else if(getStreetName(i)==two_streets[1]){two_streets_id[1]=i;}
+    }
+    intersection_ids = find_intersections_of_two_streets(make_pair(two_streets_id[0], two_streets_id[1]));
+    highlight_intersections(intersection_ids, g);
 }
                                    
 void highlight_intersections(vector<int> intersection_ids, ezgl::renderer *g){
@@ -143,7 +134,7 @@ void highlight_intersections(vector<int> intersection_ids, ezgl::renderer *g){
         g->fill_arc({x_from_lon(intersection_position.lon()), y_from_lat(intersection_position.lat())}, 10, 0, 360);
         cout<< "*******************Intersection "<<i+1 <<" ******************"<<endl
             <<"Intersection id :           "<<endl<< intersection_ids[i]<< endl
-            <<"Intersection street names:  ";
+            <<"Intersection street names:  "<<endl;
         vector<string> street_names =  find_street_names_of_intersection(intersection_ids[i]);
         for(int j=0;j<street_names.size();j++){
             cout<< street_names[j]<<endl;
@@ -158,4 +149,88 @@ void highlight_intersections(vector<int> intersection_ids, ezgl::renderer *g){
     if(YOrN =='Y'){
         g->set_visible_world(recover_screen);
     }
+}
+
+string drawFindSearchBar(ezgl::application *application){
+    ezgl::renderer *g = application->get_renderer();
+    string find_typed = "";
+    g->set_coordinate_system(ezgl::SCREEN);
+    rectangle map_full_screen = g->get_visible_screen();
+    double search_bar_width = 40;
+    double offset = 10;
+    
+    const ezgl::point2d start_point(map_full_screen.m_first.x + offset, map_full_screen.m_first.y + offset);
+    //const ezgl::point2d end_point(map_full_screen.m_second.x - offset, map_full_screen.m_first.y + search_bar_width);
+    const ezgl::point2d end_point(400, map_full_screen.m_first.y + search_bar_width);
+    
+    g->set_color(197,197,197);
+    g->draw_rectangle(start_point, end_point);
+    g->set_color(ezgl::WHITE);
+    g->fill_rectangle(start_point, end_point);
+    
+    // Sets text box to write from left instead of center
+    // This may need to be moved into main function
+    text_just write_text_left = text_just::left;
+    g->set_horiz_text_just(write_text_left);    
+    
+    if (char_pressed == true){
+        // Determine position of text box
+        ezgl::point2d text_start(start_point.x + offset, start_point.y + search_bar_width * 0.4); 
+        
+        // If backspace is pressed, delete last index of string
+        if (backspace_pressed == true){
+            if (find_typed.length() > 0){
+                find_typed.pop_back();
+                g->set_font_size(14);
+                g->set_color(ezgl::BLACK);
+                g->draw_text(text_start, typed);
+                // Reset backspace
+                backspace_pressed = false;
+            }
+        } 
+        else if (enter_pressed == true){
+            if (find_typed.length() > 0){
+                vector<int> results = find_street_ids_from_partial_street_name(typed);
+                if(results.size()!=1){
+                    application->update_message( "Your input does not uniquely identify a street. Try again.");
+                }
+                else if(results.size()==1){
+                    return find_typed;
+                }
+                enter_pressed = false;
+            }
+        }
+        else if (space_pressed == true){
+            find_typed += " ";
+            space_pressed = false;
+            g->draw_text(text_start, find_typed);
+        }
+        else {
+        // Convert character to a length one string
+            std::string temp(1, *char_print);
+            find_typed += temp;
+
+            g->set_font_size(14);
+            g->set_color(ezgl::BLACK);
+            g->draw_text(text_start, find_typed);
+            vector<int> results = find_street_ids_from_partial_street_name(find_typed);
+            int results_num = results.size();
+            if(results.size()!=1){
+                    application->update_message( "Your input does not uniquely identify a street. Try again.");
+            }
+            else if(results.size()==1){
+                return find_typed;
+                
+            }
+            for (int i = 0; i < std::max(5, results_num); i++){
+                g->set_color(ezgl::WHITE);
+                g->fill_rectangle({start_point.x, start_point.y+15*(i+1)}, {end_point.x, end_point.y+15*(i+1)});
+                g->set_color(ezgl::BLACK);
+                g->draw_text({text_start.x, text_start.y+15*(i+1)}, getStreetName(results[i]));
+            }
+        }
+        char_pressed = false;
+    }
+    // Set coordinate system back for safety
+    g->set_coordinate_system(ezgl::WORLD);
 }
